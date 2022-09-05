a287541563bf11e27cd7d8f96f823a58f75363f1
William Cirino <105467849+WilliamCirino@users.noreply.github.com>
2022-09-05 19:48:46 -0300

    Add files via upload

---
+++ RestoDivisao.java
@@ -0,0 +1,26 @@
+package exemplo;
+
+import java.util.Scanner;
+
+public class RestoDivisao {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Digite um valor.");
+
+		float valor1 = sc.nextFloat();
+
+		System.out.println("Digite um valor.");
+
+		float valor2 = sc.nextFloat();
+		
+		float resultado = valor1%valor2;
+		
+		System.out.println("O resto da Divisão é de: "+resultado);
+		
+		sc.close();
+
+	}
+
+}
