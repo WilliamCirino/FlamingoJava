1f3a212861208bad2229c851434d7bbf56c00f13
William Cirino <105467849+WilliamCirino@users.noreply.github.com>
2022-09-06 20:50:57 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,24 @@
+package exemplo;
+
+public class Variaveis {
+
+	static void mostrar() {
+		int valor = 45;
+		System.out.println("O valor da variavel é :" + valor);
+	}
+
+	public static void main(String[] args) {
+
+		int data = 50; // Variável de Instância
+		System.out.println("O valor da variável data é :" + data);
+
+		exibir();
+
+	}
+
+	static void exibir() {
+		int valor = 40;
+		System.out.println("O valor da variavel é :" + valor);
+	}
+
+}
