9dda8c6d58233957ff096df1ab7e058fba60bd4b
William Cirino <105467849+WilliamCirino@users.noreply.github.com>
2022-09-06 21:07:39 -0300

    Add files via upload

---
+++ Narrowcasting.java
@@ -0,0 +1,14 @@
+package exemplo;
+
+public class Narrowcasting {
+
+	public static void main(String[] args) {
+
+		float valor=10.5434567f;
+		int valor1=(int)valor;
+		System.out.println("O valor flutuante é :"+valor);
+		System.out.println("O valor inteiro é :"+valor1);
+
+}
+
+}
